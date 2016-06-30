bed8a473a7f5a1d13c24f21a225dd47046f432d5
卓春盛 <zhuochunsheng@zhuochunshengdeimac.local>
2016-06-30 20:37:38 +0800

    Initial Commit

---
+++ LogText/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LogText
+//
+//  Created by zhuochunsheng on 16/6/30.
+//  Copyright © 2016年 fenda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
